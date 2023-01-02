0438b37d924e8d50e7691789f1493020fd01f2ab
utkinahata <Amihajlov580@gmail.com>
2023-01-02 23:30:35 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
 
       return(
         <div className="bg-dark text-white">
-          <h1 className="text-center display-1 py-4">Rick and Morty</h1>
+          <h1 className="text-center display-1 py-4">Rick and Morty site</h1>
 
           <CharacterList/>
         
